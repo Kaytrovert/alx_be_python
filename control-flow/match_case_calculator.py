3e9795352f3ce9891d7cc6f827ea60fb9f1a2c82
KosiWills <kaytrovert@gmail.com>
2025-11-15 10:25:52 +0100

    corrections

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,16 +2,17 @@ num1 = int(input('Enter the first number: '))
 num2 = int(input('Enter the second number: '))
 operation = input('Choose the operation (+, -, *, /):')
 
-if operation == '+':
-    print('The result is ' + str((num1 + num2)))
-elif operation == '-':
-    print('The result is ' + str((num1 - num2)))
-elif operation == '*':
-    print('The result is ' + str((num1 * num2)))
-elif operation == '/':
-    if num2 == 0:
-        print('Cannot divide by zero.')
-    else:
-        print('The result is ' + str((num1 / num2)))
-else:
-    print('Invalid Operation')
+match operation:
+    case '+':
+        print('The result is ' + str(num1 + num2))
+    case '-':
+        print('The result is ' + str(num1 - num2))
+    case '*':
+        print('The result is ' + str(num1 * num2))
+    case '/':
+        if num2 == 0:
+            print('Cannot divide by zero.')
+        else:
+            print('The result is ' + str(num1 / num2))
+    case _:  
+        print('Invalid Operation')
--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,5 @@ number = int(input('Enter a number to see its multiplication table: '))
 
 for numbersss in range(1, 11):
     result = number * numbersss
-    print(str(number) + ' * ' + str(numbersss) + ' = ' + str(result))
+    print(f"{number} * {numbersss} = {result}")
+    # print(str(number) + ' * ' + str(numbersss) + ' = ' + str(result))
